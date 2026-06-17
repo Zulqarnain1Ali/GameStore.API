debc2cdf6dceab8dc3183bf73e5abf78476bd8b9
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-06-17 20:46:13 +0500

    Init GameStore API with base config, appsettings, and documentation

--- Program.cs
+++ Program.cs
@@ -1,23 +1,6 @@
-// var builder = WebApplication.CreateBuilder(args);
-// var app = builder.Build();
-
-// app.MapGet("/", () => "Hello World!");
-
-// app.Run();
-
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// 1. In-Memory Database (Simulating SQL server for now)
-var games = new List<Game>
-{
-    new Game(1, "Elden Ring", "RPG", 59.99M),
-    new Game(2, "Cyberpunk 2077", "Action", 49.99M),
-    new Game(3, "FIFA 26", "Sports", 69.99M)
-};
+app.MapGet("/", () => "Hello World!");
 
-// 2. GET: Fetch all games
-app.MapGet("/games", () => Results.Ok(games));
-
+app.Run();
