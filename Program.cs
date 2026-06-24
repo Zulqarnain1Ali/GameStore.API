d9c57c4f93b54f5ba5cadcbd3cb83714ebe39bc1
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-06-24 22:01:49 +0500

    Update program.cs and combining sqlite db

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddValidation();
 
 var connString = "Data Source = GameStore.db";
 builder.Services.AddSqlite<GameStoreContext>(connString);
-
 var app = builder.Build();
 
 app.MapGamesEndpoints();
