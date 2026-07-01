109084a9a56f9f6fcef975c13f475ed2ddaec1b5
Zulqarnain Ali Natiq <zulqarnain15ali@gmail.com>
2026-07-01 22:32:19 +0500

    Fix property casing in Game model and update migration files

---
+++ Data/Migrations/20260701172955_FixGameModelProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGameModelProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Games",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Pice",
+                table: "Games",
+                newName: "Price");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Games",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Games",
+                newName: "Pice");
+        }
+    }
+}
